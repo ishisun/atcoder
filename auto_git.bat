746e9f1c7b57aedd3eae3943bf2b4f747ee8203e
ishisun <d.logicool.a@gmail.com>
2021-04-18 15:01:32 +0900

    auto commit

--- auto_git.bat
+++ auto_git.bat
@@ -1,6 +1,6 @@
 cd C:\Users\Shun\github\atcoder
 git init
-git config --global user.name "I_S"
+git config --global user.name "ishisun"
 git config --global user.email "d.logicool.a@gmail.com"
 git clone https://github.com/ishisun/atcoder C:\Users\Shun\github\atcoder\atcoder
 xcopy /d C:\Users\Shun\Documents\atcoder C:\Users\Shun\github\atcoder\atcoder
